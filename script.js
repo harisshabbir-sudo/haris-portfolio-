34a6b2c61636e30d5ccce5ad79bcf41345187ee1
harisshabbir-sudo <harisshabbir198@gmail.com>
2026-06-08 23:10:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Thank you for visiting my portfolio!";
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+}
